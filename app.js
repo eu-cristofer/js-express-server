98e546f4b77915fd307fc6ffd0b5c3d9584e3e49
Cristofer Antoni Souza Costa <cristofer@petrobras.com.br>
2022-07-12 13:39:07 -0300

    isAuthorized function added

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
+function isAuthorized(req, res, next) {
+  const auth = req.headers.authorization;
+  if (auth === 'password') {
+    next();
+  } else {
+    res.status(401);
+    res.send('Not permitted');
+  }
+};
+
+// Configuração para a rota /
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// Configurando uma rota com retorno de um JSON
+app.get("/products", isAuthorized, (req,res) => {
+  const products = [
+  {
+    id: 1,
+    name: "hammer",
+  },
+  {
+    id: 2,
+    name: "screwdriver",
+  },
+  {
+    id: 3,
+    name: "wrench",
+  },
+ ];
+
+ res.json(products);
+});
+
+// Inicialização do aplciativo Web
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
